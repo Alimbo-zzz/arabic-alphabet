1cd1f5a166060c3694555b79e4470dfab3d8fb32
Alim Mambetov <alimbo333@gmail.com>
2024-01-29 19:04:16 +0300

    fix checkbox v2

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import cls from './style.module.scss'
 import classNames from 'classnames';
-import sprite from './sprite.svg';
+// import sprite from './sprite.svg';
+import tick from './tick-bold.svg';
 
 
 function Checkbox ({className='', ...props}) {
@@ -10,12 +11,7 @@ function Checkbox ({className='', ...props}) {
 		<label className={classNames([cls.checkbox, className])}>
 			<input className={cls.checkbox__inp} type="checkbox" {...props}/>
 			<div className={cls.checkbox__style} >
-				<svg className={cls.checkbox__tick}>
-					<use 
-						xlinkHref={`${sprite}#${'tick-bold'}`} 
-						href={`${sprite}#${'tick-bold'}`} 
-					/>
-				</svg>
+				<img src={tick} className={cls.checkbox__tick}/>
 			</div>
 		</label>
 	</>);
